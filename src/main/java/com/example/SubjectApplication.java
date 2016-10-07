83da49ebe25eff19cc7fde7901aea29263619e1f
Cheok Swin Voon <cheoksv@my.ibm.com>
2016-10-07 20:59:56 +0800

    typo

--- src/main/java/com/example/SubjectApplication.java
+++ src/main/java/com/example/SubjectApplication.java
@@ -9,6 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Lab4SubjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab4SubjectApplication.class, args);
+		SpringApplication.run(SubjectApplication.class, args);
 	}
 }
